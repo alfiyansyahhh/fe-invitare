dc711d5fbda6abe816c371fff183ea804f6eaaed
ilham_alfiyansyah <muhamadilhamalfiyansyah@gmail.com>
2025-12-03 17:12:14 +0700

    .

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,7 +12,7 @@ function Input({
       type={type}
       data-slot='input'
       className={cn(
-        'file:text-foreground focus-visible:ring-[1px] placeholder:text-muted-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-[4px] border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'file:text-foreground focus-visible:ring-[1px] placeholder:text-muted-foreground dark:bg-input/30 border-black flex h-9 w-full min-w-0 rounded-[4px] border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
         {
           'border-red-500 focus-visible:border-red-500 focus-visible:ring-red-500 ':
             isError,
